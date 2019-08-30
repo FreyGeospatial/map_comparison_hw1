6201fdd658c751b727da8067cdb36495831fd5fa
jmfrey <jfrey@clarku.edu>
2019-08-30 14:24:37 -0400

    finished plotting and reclassifying images

--- jfrey_hw1_mc.R
+++ jfrey_hw1_mc.R
@@ -6,11 +6,22 @@ lc_1971 <- raster("Landcover01/1971Landcover01.rst")
 lc_1985 <- raster("Landcover01/1985Landcover01.rst")
 lc_1999 <- raster("Landcover01/1999Landcover01.rst")
 
+# look at data
+plot(lc_1971)
+legend("topright", legend = c("Built", "Barren", "Forest", "Water"), fill = c("red", "yellow", "green", "blue"))
+
 # reclassification matrix: from, to, becomes. 2 becomes non-barren
 barren <- c(2, 3, 1)
 non_barren <- rbind(c(1, 2, 2), c(3, 5, 2))
 my_reclass <- rbind(barren, non_barren)
 
-boolean <- reclassify(lc_1971, rcl = my_reclass)
-plot(boolean)
-legend("topright", legend = c("Built", "Barren", "Forest", "Water"), fill = c("red", "yellow", "green", "blue"))
+bool_1975 <- reclassify(lc_1971, rcl = my_reclass)
+plot(bool_1975, main = "Barren and Non-Barren Land Cover: 1975")
+
+bool_1985 <- reclassify(lc_1985, rcl = my_reclass)
+plot(bool_1985, main = "Barren and Non-Barren Land Cover: 1985")
+
+bool_1999 <- reclassify(lc_1999, rcl = my_reclass)
+plot(bool_1999, main = "Barren and Non-Barren Land Cover: 1999")
+
+# cross tabulation
