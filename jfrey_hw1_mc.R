6fd3f557a6a319a7fea09b54bd1fd3813b0829f2
jmfrey <jfrey@clarku.edu>
2019-08-30 14:18:19 -0400

    plot og image and add legend. finish reclassification for boolean

--- jfrey_hw1_mc.R
+++ jfrey_hw1_mc.R
@@ -5,3 +5,12 @@ library(raster)
 lc_1971 <- raster("Landcover01/1971Landcover01.rst")
 lc_1985 <- raster("Landcover01/1985Landcover01.rst")
 lc_1999 <- raster("Landcover01/1999Landcover01.rst")
+
+# reclassification matrix: from, to, becomes. 2 becomes non-barren
+barren <- c(2, 3, 1)
+non_barren <- rbind(c(1, 2, 2), c(3, 5, 2))
+my_reclass <- rbind(barren, non_barren)
+
+boolean <- reclassify(lc_1971, rcl = my_reclass)
+plot(boolean)
+legend("topright", legend = c("Built", "Barren", "Forest", "Water"), fill = c("red", "yellow", "green", "blue"))
