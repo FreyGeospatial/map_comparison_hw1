d77b0b2bc0a4c4e9246c5f05f97a92c69a14dcb1
jmfrey <jfrey@clarku.edu>
2019-08-30 13:50:12 -0400

    load data

---
+++ jfrey_hw1_mc.R
@@ -0,0 +1,7 @@
+# load packages
+library(raster)
+
+# load data
+lc_1971 <- raster("Landcover01/1971Landcover01.rst")
+lc_1985 <- raster("Landcover01/1985Landcover01.rst")
+lc_1999 <- raster("Landcover01/1999Landcover01.rst")
