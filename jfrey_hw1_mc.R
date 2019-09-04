140a9737248776d6aecb8b69627aaf59cba3bde0
jmfrey <jfrey@clarku.edu>
2019-09-04 13:57:42 -0400

    added cross-tab visualization functionality. Reference code found at:
    https://gis.stackexchange.com/questions/329664/converting-output-of-crosstab-in-r-to-raster-from-classified-raster-images-to
    
    Also committing TerrSet cross-tab images for comparison of methods

--- jfrey_hw1_mc.R
+++ jfrey_hw1_mc.R
@@ -1,5 +1,6 @@
 # load packages
 library(raster)
+library(rasterVis)
 
 # load data
 lc_1971 <- raster("Landcover01/1971Landcover01.rst")
@@ -26,7 +27,32 @@ bool_1999 <- reclassify(lc_1999, rcl = my_reclass)
 plot(bool_1999, main = "Barren and Non-Barren Land Cover: 1999")
 
 # cross tabulation
-crosstab(bool_1971, bool_1985)
+crosstab(bool_1971, bool_1985, long = T)
 crosstab(bool_1985, bool_1999)
 
-plot(lc_1999, reclassify(lc_1999, rcl = my_reclass))
+#return all unique value combinations of these two vectors
+grid_ <- expand.grid(unique(bool_1971), unique(bool_1985))
+# change column names of value combinations
+names(grid_) <- c("from", "to")
+# add ID column to grid_
+grid_$code <- 1:nrow(grid_)
+# add change column to grid
+grid_$change <- grid_[,1] != grid_[,2]
+
+change <- function(x){
+  grid_[x[1] == grid_[,1] & x[2] == grid_[,2],'code']
+}
+
+# calculate values for a new raster object from another raster object using a formula
+changeDet1 <- calc(stack(bool_1971,bool_1985), fun = change)
+
+# dictionary
+codes_ <- data.frame(ID = grid_$code,value = paste0('from ',grid_[,1],' to ',grid_[,2]))
+
+# Create a Raster Attribute Table
+rat[["Changes"]] <- codes_
+levels(changeDet1) <- rat
+# Plot
+levelplot(changeDet1, par.settings=PuOrTheme(), xlab="", ylab="")
+
+
