d59226fbd407b9ff2d251e5a8ed7c9561e7bb2b4
jmfrey <jfrey@clarku.edu>
2019-09-04 16:55:51 -0400

    added cross tab map for second plot, and added titles

--- jfrey_hw1_mc.R
+++ jfrey_hw1_mc.R
@@ -27,32 +27,44 @@ bool_1999 <- reclassify(lc_1999, rcl = my_reclass)
 plot(bool_1999, main = "Barren and Non-Barren Land Cover: 1999")
 
 # cross tabulation
-crosstab(bool_1971, bool_1985, long = T)
+crosstab(bool_1971, bool_1985)
 crosstab(bool_1985, bool_1999)
 
-#return all unique value combinations of these two vectors
+#return all unique value combinations of these two vectors. works with both raster images as they have same unique values
 grid_ <- expand.grid(unique(bool_1971), unique(bool_1985))
 # change column names of value combinations
 names(grid_) <- c("from", "to")
 # add ID column to grid_
 grid_$code <- 1:nrow(grid_)
-# add change column to grid
-grid_$change <- grid_[,1] != grid_[,2]
+# add change column to grid. Anything that doesnt equal is given True val
+grid_$change <- grid_$from != grid_$to
 
+# change detection function to add dictionary code values.
+# This function acts on two raster images contained within a stack, which
+# is represented by x
 change <- function(x){
-  grid_[x[1] == grid_[,1] & x[2] == grid_[,2],'code']
+  grid_[x[1] == grid_$from & x[2] == grid_$to,'code']
 }
 
 # calculate values for a new raster object from another raster object using a formula
-changeDet1 <- calc(stack(bool_1971,bool_1985), fun = change)
+# notice that the two rasters in question are merged into a stack
+
+changeDet1 <- calc(stack(bool_1971, bool_1985), fun = change)
+changeDet2 <- calc(stack(bool_1985,bool_1999), fun = change)
 
 # dictionary
 codes_ <- data.frame(ID = grid_$code,value = paste0('from ',grid_[,1],' to ',grid_[,2]))
+#give raster categorical factor levels
+levels(changeDet1) <- codes_
+# Plot -- levelplot plots the factor levels as a legend, whereas plot() would be continuous
+levelplot(changeDet1, 
+          par.settings=PuOrTheme(), 
+          xlab="", ylab="",
+          main = "1971-85 Change Detection")
 
-# Create a Raster Attribute Table
-rat[["Changes"]] <- codes_
-levels(changeDet1) <- rat
-# Plot
-levelplot(changeDet1, par.settings=PuOrTheme(), xlab="", ylab="")
-
+levels(changeDet2) <- codes_
+levelplot(changeDet2, 
+          par.settings=PuOrTheme(), 
+          xlab="", ylab="",
+          main = "1985-99 Change Detection")
 
